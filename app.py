e1b601bc85fb0132404eb15a3057d04a93884591
ShehabAmr <shehabamr103@gmail.com>
2025-05-23 23:22:22 +0300

    Modify

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ def predict():
     return render_template("index.html", prediction_text=f"Your Loan is: {status}")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
